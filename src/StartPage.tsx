79e82ff0b502556a534f30d64dc59db2aecaa03b
Jay <74604447+jasonprogrammer775@users.noreply.github.com>
2023-02-24 14:16:16 +0100

    2nd

--- src/StartPage.tsx
+++ src/StartPage.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const StartPage = () => {
   return (
     <Container>
-      <h1>Hello World</h1>
+      <h1>Hello User</h1>
       <Link to="todos">
         <Button variant="contained">Go to todo list</Button>
       </Link>
